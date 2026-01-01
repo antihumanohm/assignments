7dcd8c10cc49120e7cdf95e76cb6a32f900977fd
antihumanohm <unloveable10753@gmail.com>
2026-01-01 16:14:29 +0700

    create file

---
+++ Exercise4_Sarun_S.py
@@ -0,0 +1,10 @@
+english = 95
+business = 85
+comSystem = 95
+comProgram = 86
+
+print("--- Your Score ---")
+print("Foundation English : คะแนนที่ได้", english,"คะแนน")
+print("General Business : คะแนนที่ได้",business,"คะแนน")
+print("Introduction to Computer Systems : คะแนนที่ได้",comSystem,"คะแนน")
+print("Computer Programming : คะแนนที่ได้",comProgram,"คะแนน")
