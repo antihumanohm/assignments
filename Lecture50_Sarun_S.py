f2e192038bb9fe64f659012f8148f682632cc7bf
antihumanohm <unloveable10753@gmail.com>
2026-01-03 09:53:52 +0700

    Create Lecture50_Sarun_S.py

---
+++ Lecture50_Sarun_S.py
@@ -0,0 +1,19 @@
+def addition(x,y):
+    print(x+y)
+
+def subtraction(x,y):
+    print(x-y)
+
+def multiplication(x,y):
+    print(x*y)
+
+def division(x,y):
+    print(x/y)
+
+x = int(input("enter first number : "))
+y = int(input("enter second number : "))
+
+addition(x,y)
+subtraction(x,y)
+multiplication(x,y)
+division(x,y)
